a96d4c69a80c7a831a30e2443bbfa9d190a1ce63
Jonathan Carter <jonathan@bluemosh.com>
2019-10-16 09:31:58 +0200

    New upstream release, add upstream files

---
+++ README.md
@@ -0,0 +1,32 @@
+Multi Monitors Add-On
+=====================
+
+Extension inspired by https://github.com/darkxst/multiple-monitor-panels 
+and rewritten from scratch for gnome-shell version 3.10.4.  Adds panels 
+and thumbnails for additional monitors. Settings changes are applied 
+in dynamic fashion, no restart needed.
+
+Versions
+========
+
+* Branch [master](https://github.com/spin83/multi-monitors-add-on/tree/master) contains extension for GNOME 3.32 and 3.34
+* Branch [gnome-3-24_3-30](https://github.com/spin83/multi-monitors-add-on/tree/master) contains extension for GNOME 3.24, 3.26, 3.28 and 3.30
+* Branch [gnome-3-20_3-22](https://github.com/spin83/multi-monitors-add-on/tree/gnome-3-20_3-22) contains extension for GNOME 3.20 and 3.22
+* Branch [gnome-3-16_3-18](https://github.com/spin83/multi-monitors-add-on/tree/gnome-3-16_3-18) contains extension for GNOME 3.16 and 3.18
+* Branch [gnome-3-14](https://github.com/spin83/multi-monitors-add-on/tree/gnome-3-14) contains extension for GNOME 3.14
+* Branch [gnome-3-10](https://github.com/spin83/multi-monitors-add-on/tree/gnome-3-10) contains extension for GNOME 3.10
+
+Installation from git
+=====================
+
+    git clone git://github.com/spin83/multi-monitors-add-on.git
+    cd multi-monitors-add-on
+    cp -r multi-monitors-add-on@spin83 ~/.local/share/gnome-shell/extensions/
+
+Restart the shell and then enable the extension.
+
+License
+=======
+
+Multi Monitors Add-On extension is distributed under the terms of the 
+GNU General Public License, version 2 or later. See the LICENSE file for details.
---
+++ multi-monitors-add-on@spin83/indicator.js
@@ -0,0 +1,135 @@
+/*
+Copyright (C) 2014  spin83
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, visit https://www.gnu.org/licenses/.
+*/
+
+const { St, Gio, GObject } = imports.gi;
+
+const Util = imports.misc.util;
+
+const Main = imports.ui.main;
+const Tweener = imports.ui.tweener;
+const PanelMenu = imports.ui.panelMenu;
+
+const Gettext = imports.gettext.domain('multi-monitors-add-on');
+const _ = Gettext.gettext;
+const CE = imports.misc.extensionUtils.getCurrentExtension();
+const Convenience = CE.imports.convenience;
+const extensionPath = CE.path;
+
+var MultiMonitorsIndicator = (() => {
+	let MultiMonitorsIndicator = class MultiMonitorsIndicator extends PanelMenu.Button {
+		_init() {
+			super._init(0.0, "MultiMonitorsAddOn", false);
+			
+			Convenience.initTranslations();
+			
+			this.text = null;
+	
+			this._mmStatusIcon = new St.BoxLayout({ style_class: 'multimonitor-status-indicators-box' });
+			this._mmStatusIcon.hide();
+			this.add_child(this._mmStatusIcon);
+			this._leftRightIcon = true;
+	
+			this.menu.addAction(_("Preferences"), this._onPreferences.bind(this));
+			
+			this._viewMonitorsId = Main.layoutManager.connect('monitors-changed', this._viewMonitors.bind(this));
+			this._viewMonitors();
+		}
+			
+		_onDestroy() {
+			Main.layoutManager.disconnect(this._viewMonitorsId);
+			super._onDestroy();
+		}
+		
+	    _syncIndicatorsVisible() {
+	        this._mmStatusIcon.visible = this._mmStatusIcon.get_children().some(a => a.visible);
+	    }
+	    
+	    _icon_name (icon, iconName) {
+	    	icon.set_gicon(Gio.icon_new_for_string(extensionPath+"/icons/"+iconName+".svg"));
+	    }
+		
+		_viewMonitors() {
+			let monitors = this._mmStatusIcon.get_children();
+		
+			let monitorChange = Main.layoutManager.monitors.length - monitors.length;
+			if(monitorChange>0){
+				global.log("Add Monitors ...");
+				for(let idx = 0; idx<monitorChange; idx++){
+					let icon;
+					icon = new St.Icon({style_class: 'system-status-icon multimonitor-status-icon'});
+					this._mmStatusIcon.add_child(icon);
+					icon.connect('notify::visible', this._syncIndicatorsVisible.bind(this));
+					
+					if (this._leftRightIcon)
+						this._icon_name(icon, 'multi-monitors-l-symbolic');
+					else
+						this._icon_name(icon, 'multi-monitors-r-symbolic');
+					this._leftRightIcon = !this._leftRightIcon;
+				}
+				this._syncIndicatorsVisible();
+			}
+			else if(monitorChange<0){
+				global.log("Remove Monitors ...");
+				monitorChange = -monitorChange;
+				
+				for(let idx = 0; idx<monitorChange; idx++){
+					let icon = this._mmStatusIcon.get_last_child();
+					this._mmStatusIcon.remove_child(icon);
+					icon.destroy();
+					this._leftRightIcon = !this._leftRightIcon;
+				}
+			}
+		}
+		
+		_onPreferences()
+		{
+			Util.spawn(["gnome-shell-extension-prefs", "multi-monitors-add-on@spin83"]);
+		}
+		
+		_onInit2ndMonitor()
+		{
+			Util.spawn(["intel-virtual-output"]);
+		}
+		
+		_hideHello() {
+		    Main.uiGroup.remove_actor(this.text);
+		    this.text = null;
+		}
+		
+		_showHello() {
+		    if (!this.text) {
+		        this.text = new St.Label({ style_class: 'helloworld-label', text: _("Multi Monitors Add-On") });
+		        Main.uiGroup.add_actor(this.text);
+		    }
+	
+		    this.text.opacity = 255;
+	
+		    let monitor = Main.layoutManager.primaryMonitor;
+	
+		    this.text.set_position(Math.floor(monitor.width / 2 - this.text.width / 2),
+		                      Math.floor(monitor.height / 2 - this.text.height / 2));
+	
+		    Tweener.addTween(this.text,
+		                     { opacity: 0,
+		                       time: 4,
+		                       transition: 'easeOutQuad',
+		                       onComplete: this._hideHello.bind(this) });
+	
+		}
+	};
+	return GObject.registerClass(MultiMonitorsIndicator);
+})();
